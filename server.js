edf127186ec84b1874e6bd1cc3f598c90efb4580
Woomymy <github@woomy.ovh>
2020-05-20 20:08:30 +0200

    New version base

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express');
+
+const app = express()
+
+app.listen(process.env.PORT || 3000, async () => {
+    console.log(`[SERVER] Listening on port ${process.env.PORT || 3000}`)
+});
+
+app.use(express.static(__dirname + '/assets'));
+app.get('/', async (req, res) => {
+    res.sendFile(`${path.join(__dirname, `./assets/index.html`)}`)
+});
