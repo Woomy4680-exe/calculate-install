e368c0ca1711837aa3ca11faa33ecb748ffeb196
Matteo Zarola <matzar1808@gmail.com>
2020-06-01 14:21:12 +0200

    Partie d'installation.ready = true

--- assets/scripts/install.js
+++ assets/scripts/install.js
@@ -3,3 +3,7 @@ let lvs = document.getElementById('lvservices');
 lvs.addEventListener('click', () => {
     lvs.style.color = 'lightgreen';
 });
+let clinstall = document.getElementById('clinstall');
+clinstall.addEventListener('click', () => {
+    alert('POPOPOPP! Copier ne sert à rien');
+});
