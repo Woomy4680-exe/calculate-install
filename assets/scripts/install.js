b83dce3a1ef0aa0f3857707b2c6039eb96315bdd
Matteo Zarola <matzar1808@gmail.com>
2020-06-01 14:01:02 +0200

    Eslint update and cool color for LVM

---
+++ assets/scripts/install.js
@@ -0,0 +1,5 @@
+console.log('Loading installation page script...');
+let lvs = document.getElementById('lvservices');
+lvs.addEventListener('click', () => {
+    lvs.style.color = 'lightgreen';
+});
